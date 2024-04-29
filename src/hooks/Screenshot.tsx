fd1b1ce4acef047c0a856987abb1fa49c6b20496
ch-hamid14 <hamidashraf3440@gmail.com>
2024-04-30 01:02:37 +0500

    add the screenshot hook

--- src/hooks/Screenshot.tsx
+++ src/hooks/Screenshot.tsx
@@ -1,9 +1,47 @@
 import React from 'react'
 
 const Screenshot = () => {
-  return (
-    <div>Screenshot</div>
-  )
+  const takeScreenShot = async () => {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      console.log('getDisplayMedia is not supported in this environment');
+      return null;
+    }
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        //@ts-ignore
+        mandatory: {
+          chromeMediaSource: 'desktop',
+          chromeMediaSourceId: '',
+          minWidth: 1280,
+          maxWidth: 1280,
+          minHeight: 720,
+          maxHeight: 720,
+        },
+      },
+    });
+    const track = stream.getVideoTracks()[0];
+    //@ts-ignore
+    const imageCapture = new ImageCapture(track);
+    const bitmap = await imageCapture.grabFrame();
+    track.stop();
+    const canvas = document.createElement('canvas');
+    canvas.width = bitmap.width;
+    canvas.height = bitmap.height;
+    const context = canvas.getContext('2d');
+    context?.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height);
+    const image = canvas.toDataURL();
+    const res = await fetch(image);
+    const buff = await res.arrayBuffer();
+    const file = [
+      new File([buff], `photo_${new Date()}.jpg`, {
+        type: 'image/jpeg',
+      }),
+    ];
+    return file;
+  };
+  takeScreenShot();
 }
 
 export default Screenshot;
